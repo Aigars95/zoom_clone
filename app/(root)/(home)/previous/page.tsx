2d06bc491c5e2cf3374983817bab6ffa1cb77d98
Aigars <aigarskaufelds@gmail.com>
2025-09-24 14:34:31 +0300

    added home, upcoming, previous, recordings, personal-room pages

---
+++ app/(root)/(home)/previous/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Previous = () => {
+    return (
+        <section className="flex size-full flex-col gap-10">
+            <h1 className="text-3xl font-bold">
+                Personal Room
+            </h1>
+        </section>
+    );
+};
+
+export default Previous;
