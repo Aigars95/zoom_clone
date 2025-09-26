e0214b119a0ad39cd9b6277ce67e9df3e6263a16
Aigars <aigarskaufelds@gmail.com>
2025-09-26 11:43:36 +0300

    added hero banner to HomePage

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -11,7 +11,7 @@ const HomeLayout = ({ children }:
             <div className="flex">
                 <Sidebar />
 
-                <section className="flex min-h-screen flex1 flex-col
+                <section className="flex min-h-screen flex-1 flex-col
                  px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
                     <div className="w-full">
                         {children}
